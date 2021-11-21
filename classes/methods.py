c214cdf309fdf93010e50fb1d41a69560f7c7cbe
TesterNick <nikolaevnnne@gmail.com>
2021-11-21 17:52:33 +0300

    Extract duplicated code to the separate method

--- classes/methods.py
+++ classes/methods.py
@@ -8,16 +8,8 @@ def get(link: str) -> tuple[requests.Response, dict, dict]:
     # Seems server don't want to work with automated requests
     # and returns 406 Not Acceptable.
     full_response = requests.get(link, headers={"User-Agent": ""})
-    try:
-        response_data = json.loads(full_response.text)
-        employee_data = response_data["data"]
-        return full_response, response_data, employee_data
-    except json.JSONDecodeError:
-        # If server returned something unexpected we want to know
-        # what happened.
-        print((f"Response status: {full_response.status_code}.\n"
-               f"Response text: {full_response.text}"))
-        raise
+    response_data, employee_data = parse_json(full_response)
+    return full_response, response_data, employee_data
 
 
 def post(link: str, data: dict = None) -> tuple[requests.Response, dict, dict]:
@@ -25,10 +17,15 @@ def post(link: str, data: dict = None) -> tuple[requests.Response, dict, dict]:
     # Seems server don't want to work with automated requests
     # and returns 406 Not Acceptable.
     full_response = requests.post(link, data=data, headers={"User-Agent": ""})
+    response_data, employee_data = parse_json(full_response)
+    return full_response, response_data, employee_data
+
+
+def parse_json(full_response: requests.Response) -> tuple[dict, dict]:
     try:
         response_data = json.loads(full_response.text)
         employee_data = response_data["data"]
-        return full_response, response_data, employee_data
+        return response_data, employee_data
     except json.JSONDecodeError:
         # If server returned something unexpected we want to know
         # what happened.
