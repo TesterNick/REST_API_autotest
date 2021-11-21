e0364b6e68d2d728555c60ffe7389504a8dfadbc
TesterNick <nikolaevnnne@gmail.com>
2021-11-21 16:35:39 +0300

    Add method for post request and parsing its response

--- classes/methods.py
+++ classes/methods.py
@@ -18,3 +18,20 @@ def get(link: str) -> tuple[requests.Response, dict, dict]:
         print((f"Response status: {full_response.status_code}.\n"
                f"Response text: {full_response.text}"))
         raise
+
+
+def post(link: str, data: dict = None) -> tuple[requests.Response, dict, dict]:
+    # Requests adds header 'User-Agent': 'python-requests/2.26.0'.
+    # Seems server don't want to work with automated requests
+    # and returns 406 Not Acceptable.
+    full_response = requests.post(link, data=data, headers={"User-Agent": ""})
+    try:
+        response_data = json.loads(full_response.text)
+        employee_data = response_data["data"]
+        return full_response, response_data, employee_data
+    except json.JSONDecodeError:
+        # If server returned something unexpected we want to know
+        # what happened.
+        print((f"Response status: {full_response.status_code}.\n"
+               f"Response text: {full_response.text}"))
+        raise
